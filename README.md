e86afe04a4f50f46b83c5285838d78496344be87
Hendi Santika <hendisantika@gmail.com>
2018-08-27 21:44:35 +0700

    #1 - first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-boot-graphql-query-example
---
+++ src/main/java/com/hendisantika/springbootgraphqlqueryexample/SpringBootGraphqlQueryExampleApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootgraphqlqueryexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraphqlQueryExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGraphqlQueryExampleApplication.class, args);
+    }
+}
